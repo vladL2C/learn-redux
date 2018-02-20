e0e569767b48605662aeaf2b79035e3d995479df
Vlad <vlad@hotmail.co.nz>
2018-02-20 16:22:55 +1300

    first

---
+++ client/components/PhotoGrid.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+const PhotoGrid = React.createClass({
+  render() {
+    return (
+      <div className="photo-grid">
+        I'm the photogrid
+      </div>
+    )
+  }
+});
+
+export default PhotoGrid;
---
+++ client/components/Single.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+const Single = React.createClass({
+  render() {
+    return (
+      <div className="single-photo">
+        Im the single
+      </div>
+    )
+  }
+});
+
+export default Single;
